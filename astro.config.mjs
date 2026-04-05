edf0daf03fd8f8794bcfd2305607b92d6d3a67ec
Bruno2204 <brunoegama@gmail.com>
2026-04-05 15:16:33 -0300

    Add: Vercel Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -13,4 +15,5 @@ export default defineConfig({
 
   output: 'server',
   integrations: [react()],
+  adapter: vercel(),
 });
